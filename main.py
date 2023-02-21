8415fb5733da8d76a81dd6eb4f04c6b77dfb6807
Nicolas Crespo <122305393+n1colas-crespo@users.noreply.github.com>
2023-02-21 12:06:05 -0500

    added

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 print("hello world")
 import subprocess
 subprocess.run('ffmpeg -i rickroll.mp4 nic.mp3', shell = True)
+
+# with stuff open
+    # write original starting HTML CODE
+
+    # define TitleVideo/TitleAudio
+    # write new starting HTML CODE
+
+    yt.streams.get_highest_resolution().download()
+    subprocess.run('ffmpeg -i' + "\""+titleV + "\"" + " " + "\"" + titleA + "\"", shell=True)
+
+    file.write("<td><a href=" + "\"" + u + "\">" + title")
+               
+    # add ending string for <tr>
+    # use import os, os.listdir instead to get correct file names
+    
